60fd1c76d02872dcb00906f1bee13ac259b8a065
Makktu <johnmcna@gmail.com>
2023-03-29 03:49:24 +0100

    tweaks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,6 +13,11 @@ const Navbar = () => {
       <Button style="export-btn" buttonText="EXPORT" />
       <Button buttonText="NEW WEEK" />
       <Button style="timer-btn" buttonText="TIMER" onClick={clickHandler} />
+      <label className="navLabel">Cell Color: </label>
+      <span className="pick-orangered"></span>
+      <span className="pick-yellow"></span>
+      <span className="pick-cyan"></span>
+      <span className="pick-gray"></span>
       <Button style="settings-btn" buttonText="SETTINGS" />
     </>
   );
