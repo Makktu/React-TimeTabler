35372e713bd92daaf3d0113084dc8089d5a1fd9d
Makktu <johnmcna@gmail.com>
2023-03-31 02:07:16 +0100

    pdated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,19 @@ import Button from "./Button";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const clickHandler = () => {
-    alert("CLICKED");
+  const clickHandler = (e) => {
+    console.log(e.target.className);
+    if (e.target.className === "timer-btn") {
+      alert("Timer functionality coming in a later version");
+    }
+    if (e.target.className === "save-btn") {
+      alert("Save functionality coming in this version, soon.");
+    }
   };
 
   return (
     <>
-      <Button buttonText="SAVE" />
+      <Button style="save-btn" buttonText="SAVE" onClick={clickHandler} />
       <Button style="export-btn" buttonText="EXPORT" />
       <Button buttonText="NEW WEEK" />
       <Button style="timer-btn" buttonText="TIMER" onClick={clickHandler} />
